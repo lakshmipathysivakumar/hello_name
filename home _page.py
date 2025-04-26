e1d2403a8d21561fb9a88fac78e6409d9440dfa7
lakshmipathysivakumar <147093539+lakshmipathysivakumar@users.noreply.github.com>
2025-04-26 14:23:05 +0530

    Create home _page.py

---
+++ home _page.py
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Enter your name</title>
+    </head>
+    <body>
+        <h2>what is your name?</h2>
+        <form method="post">
+            <input type="text" name="name" required>
+            <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
